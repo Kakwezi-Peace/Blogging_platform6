46427d9b4d211dbe71705af62a9a4764b199a981
kakwezi peace <peacekakwezi222@gmail.com>
2026-02-11 08:42:34 +0200

    Add Activity entity

--- src/main/java/com/example/Blogging_platform2/model/ActivityLog.java
+++ src/main/java/com/example/Blogging_platform2/model/ActivityLog.java
@@ -19,20 +19,18 @@ public class ActivityLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //  will Link to User (who performed the action)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(nullable = false)
-    private String action;   // e.g., "CREATE_POST", "DELETE_COMMENT"
+    private String action;   // "CREATE_POST", "DELETE_COMMENT"
 
-    // Optional link to a target entity (like Post or Comment)
     @Column(name = "target_id")
     private Long targetId;
 
     @Column(columnDefinition = "TEXT")
-    private String details;  // extra info about the action or more details
+    private String details;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
