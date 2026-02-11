7f2e2e3f63a5b4b229eab5596f475a6ca1ea6b41
kakwezi peace <peacekakwezi222@gmail.com>
2026-02-11 09:26:35 +0200

    Create A webconfiguration class

--- src/main/java/com/example/Blogging_platform2/config/WebConfig.java
+++ src/main/java/com/example/Blogging_platform2/config/WebConfig.java
@@ -1,5 +1,4 @@
 package com.example.Blogging_platform2.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
