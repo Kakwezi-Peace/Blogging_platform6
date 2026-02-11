f2b25a697337c0d51b806145eefdb4a4ac3cc18d
kakwezi peace <peacekakwezi222@gmail.com>
2026-02-11 09:10:01 +0200

    update UserDto class

--- src/main/java/com/example/Blogging_platform2/dto/UserDto.java
+++ src/main/java/com/example/Blogging_platform2/dto/UserDto.java
@@ -1,5 +1,4 @@
 package com.example.Blogging_platform2.dto;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
