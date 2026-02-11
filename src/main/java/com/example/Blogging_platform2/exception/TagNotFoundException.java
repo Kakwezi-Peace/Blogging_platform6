94dc96bbdc0cad2319bfc6cb13b5306614fb099a
kakwezi peace <peacekakwezi222@gmail.com>
2026-02-11 09:01:45 +0200

    made a TagNotFoundException class

--- src/main/java/com/example/Blogging_platform2/exception/TagNotFoundException.java
+++ src/main/java/com/example/Blogging_platform2/exception/TagNotFoundException.java
@@ -1,6 +1,5 @@
 package com.example.Blogging_platform2.exception;
 public class TagNotFoundException extends RuntimeException {
-
     public TagNotFoundException(Long id) {
         super("Tag with ID " + id + " not found");
     }
