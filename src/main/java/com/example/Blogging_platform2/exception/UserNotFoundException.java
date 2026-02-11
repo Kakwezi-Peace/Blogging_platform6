964897b17c7f18e792241e044c65924dffb839b7
kakwezi peace <peacekakwezi222@gmail.com>
2026-02-11 09:01:21 +0200

    made a UserNotFoundException class

--- src/main/java/com/example/Blogging_platform2/exception/UserNotFoundException.java
+++ src/main/java/com/example/Blogging_platform2/exception/UserNotFoundException.java
@@ -4,7 +4,6 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
         super("User with ID " + id + " not found");
     }
-
     public UserNotFoundException(String message) {
         super(message);
     }
